8051f44c11d6a45e4e041e8f63577b806f7fad18
Ilya060469 <75317087+Ilya060469@users.noreply.github.com>
2021-04-20 19:53:29 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,104 @@
+import traceback
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()  # берем все что есть из базы данных и сортируем по дате
+    return render_template("posts.html", articles=articles)  # передаем список articles в posts.html
+
+
+@app.route('/posts/<int:id>')
+def posts_det(id):
+    article = Article.query.get(id)
+    return render_template("posts_det.html", article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении произошла ошибка", traceback.format_exc()
+
+
+@app.route('/posts/<int:id>/re', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return 'При редактировании произошла ошибка'
+    else:
+        return render_template("post_update.html", article=article)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return traceback.format_exc()
+    else:
+        return render_template("create-article.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page:" + name + ' - ' + str(id)
+
+@app.route('/politics')
+def cum():
+    return render_template('politics.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
